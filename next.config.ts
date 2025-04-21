02bbfb7b8acf05f6cff964ed5a9281b2be4cd6c4
Suwashshrestha <suwashshrestha5536@gmail.com>
2025-04-21 11:57:56 +0545

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Makes the app statically exportable
+  output: 'export',
+  distDri: 'dist', // Makes the app statically exportable
   images: {
     unoptimized: true, // Fixes Next.js image issues on static
   },
